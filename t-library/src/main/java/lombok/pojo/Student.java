e4d4d86694b6ea54b037df4df16278e3c6d60994
许磊 <xulei131401@outlook.com>
2023-01-20 20:42:30 +0800

    fix

---
+++ t-library/src/main/java/lombok/pojo/Student.java
@@ -0,0 +1,15 @@
+package lombok.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @NonNull
+    private String name;
+    private int age;
+}
---
+++ t-library/src/test/java/com/xulei/ehcache/MyTest.java
@@ -0,0 +1,10 @@
+package com.xulei.ehcache;
+
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test(){
+
+    }
+}
